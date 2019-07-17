158c4d64e5f29b1cb66cf4b0981956f47efb770f
martin <52429631+martindsosa@users.noreply.github.com>
2019-07-16 22:14:22 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,9 +2,36 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño){
+    case "Enero":{
+       
+    }
+    case "Febrero":{
+       
+    }
+    case "Marzo":{
+    }
+    case "Abril":{
+    }
+    case "Mayo":{
 
-alert (mesDelAño);
-
+    }
+    case "Junio":{
+        alert("Falta para el invierno ");
+        break;
+    }
+    case "Julio":{
+        
+    }
+    case "Agosto":{
+        alert("Abrigate que hace frio ");
+        break;
+    }
+    default: {
+        alert ("Ya pasamos el frio, ahora calor!!!");
+        break;
+    } 
+}
 
 
 
