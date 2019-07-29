79a6bdd311581f098a89342f248726258aeb2428
martin <52429631+martindsosa@users.noreply.github.com>
2019-07-29 17:33:42 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,56 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+    var descuento;
+    //var descuentoSobrePrecio;
+    var cantidadLamparas = document.getElementById("Cantidad").value;
+    cantidadLamparas = parseInt(cantidadLamparas);
+
+    var precioSinDescuento = 35 * cantidadLamparas;
+
+    if (cantidadLamparas >= 6 ){
+    
+        descuento = precioSinDescuento * 0.5;
+        precioSinDescuento = precioSinDescuento - descuento;
+    }else if(cantidadLamparas == 5){
+        var marcaLamparas = document.getElementById("Marca").value;
+        if (marcaLamparas == "ArgentinaLuz"){
+            descuento = precioSinDescuento * 0.4;
+            precioSinDescuento = precioSinDescuento - descuento;
+        }
+        else{
+            descuento = precioSinDescuento * 0.3;
+            precioSinDescuento = precioSinDescuento - descuento;
+        }
+    }else if (cantidadLamparas == 4){
+        var marcaLamparas = document.getElementById("Marca").value;
+        if (marcaLamparas == "ArgentinaLuz" || marcaLamparas == "FelipeLamparas"){
+            descuento = precioSinDescuento * 0.25;
+            precioSinDescuento = precioSinDescuento - descuento;
+        }
+        else{
+            descuento = precioSinDescuento * 0.20; 
+            precioSinDescuento = precioSinDescuento - descuento;
+        }
+    }else if (cantidadLamparas == 3){
+        var marcaLamparas = document.getElementById("Marca").value;
+        if (marcaLamparas == "ArgentinaLuz"){
+            descuento = precioSinDescuento * 0.15;
+            precioSinDescuento = precioSinDescuento - descuento;
+        }
+        else if(marcaLamparas == "FelipeLamparas"){
+            descuento = precioSinDescuento * 0.10;
+            precioSinDescuento = precioSinDescuento - descuento;
+        }
+        else{
+            descuento = precioSinDescuento * 0.05; 
+            precioSinDescuento = precioSinDescuento - descuento;
+        }
+    }
+    if (precioSinDescuento > 120){
+        var ingresoBruto = precioSinDescuento * 0.1;
+        precioSinDescuento = precioSinDescuento + ingresoBruto;
+        alert ("IIBB Usted pago " + ingresoBruto);
+    }
+    document.getElementById("precioDescuento").value = precioSinDescuento;
 }
