1f58afa9abeff4f35dd3d41e8fe188b47247ae37
martin <52429631+martindsosa@users.noreply.github.com>
2019-07-29 17:35:52 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+    var planeta = prompt("Ingrese planeta del sistema solar");
+
+    switch(planeta){
+        case "tierra":{
+            alert ("aca vivimos");
+            break;
+        }
+        case "venus":
+        case "mercurio":{
+            alert("aca hace mas calor");
+            break;
+        }
+        case "marte":
+        case "jupiter":
+        case "saturno":
+        case "urano":
+        case "neptuno":{
+            alert("aca hace mas frio");
+            break;
+        }
+        default:
+            alert("este planeta no es valido");
+            break;
+    }
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
-
+    var resultado;
+    var numeroUno = prompt("Ingrese primer numero ","0");
+    numeroUno = parseInt(numeroUno);
+    var numeroDos = prompt("Ingrese segundo numero ","0");
+    numeroDos = parseInt(numeroDos);
+    if (numeroUno == numeroDos){
+        alert (numeroUno.toString() + numeroDos.toString());
+    }else if (numeroUno > numeroDos){
+        resultado = numeroUno - numeroDos;
+        alert("La resta de los numero es " + resultado);        
+    }
+    resultado = numeroDos + numeroUno;
+    if (resultado > 10){
+        alert("la suma es " + resultado + " y supero el 10")
+    }
+    
 }
