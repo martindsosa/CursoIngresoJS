c057c09aeb087aae5e0bb798f94de5a7f5456390
martin <52429631+martindsosa@users.noreply.github.com>
2019-07-29 17:37:16 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,43 +1,26 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño = document.getElementById('mes').value;
 
-switch(mesDelAño){
-    case "Enero":{
-
-    }
-    case "Marzo":{
-
-    }
-    case "Mayo":{
-
-    }
-    case "Julio":{
-
-    }
-    case "Agosto":{
-
-    }
-    case "Octubre":{
-
-    }
-    case "Diciembre":{
-        alert ("Este mes tiene 31 dias");
-        break;
-    }
-    case "Febrero":{
-        alert("Este mes tiene 28 dias");
-        break;
-    }
-    default:
-    {
-        alert("Este mes tiene 30 dias");
-        break;
-    }
-}	
-	
-
-
-
-}//FIN DE LA FUNCIÓN
+    switch(mesDelAño){
+        case "Enero":
+        case "Marzo":
+        case "Mayo":
+        case "Julio":
+        case "Agosto":
+        case "Octubre":
+        case "Diciembre":{
+            alert ("Este mes tiene 31 dias");
+            break;
+        }
+        case "Febrero":{
+            alert("Este mes tiene 28 dias");
+            break;
+        }
+        default:
+        {
+            alert("Este mes tiene 30 dias");
+            break;
+        }
+    }	
+}
